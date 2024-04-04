479fbb25df3fb05f0a33538387a4515d39796b27
harpesh <khambhaliyaharpesh@gmail.com>
2024-04-04 22:12:44 +0530

    second commit

--- api/models.py
+++ api/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-
+class Student(models.model):
+    name = models.models.CharField(max_length=50)
+    lastname = models.models.CharField(max_length=50)
